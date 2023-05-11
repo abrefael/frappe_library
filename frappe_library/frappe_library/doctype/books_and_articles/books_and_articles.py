2086b90dcb2246e3db054a755f690671b6b9f70d
Alon Ben Refael <alon.ben.refael@gmail.com>
2023-05-11 10:15:47 +0000

    new file:   frappe_library/frappe_library/doctype/__init__.py 	new file:   frappe_library/frappe_library/doctype/authors/__init__.py 	new file:   frappe_library/frappe_library/doctype/authors/authors.json 	new file:   frappe_library/frappe_library/doctype/authors/authors.py 	new file:   frappe_library/frappe_library/doctype/books_and_articles/__init__.py 	new file:   frappe_library/frappe_library/doctype/books_and_articles/books_and_articles.js 	new file:   frappe_library/frappe_library/doctype/books_and_articles/books_and_articles.json 	new file:   frappe_library/frappe_library/doctype/books_and_articles/books_and_articles.py 	new file:   frappe_library/frappe_library/doctype/books_and_articles/test_books_and_articles.py 	modified:   requirements.txt

---
+++ frappe_library/frappe_library/doctype/books_and_articles/books_and_articles.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2023, me and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class BooksandArticles(Document):
+	pass
+
+@frappe.whitelist()
+def guess_pdf_data(pdf_file_path = None, doi = None, isbn = None):
+	import requests
+	return 'Something'
